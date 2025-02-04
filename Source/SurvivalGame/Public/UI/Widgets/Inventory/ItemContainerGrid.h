5ffb77837c180c677f0a4119db682bbe5968a2d1
Halit Bilici <hbilici@me.bergen.edu>
2025-02-04 03:46:04 -0500

    New game icons, new borders and button styles added

---
+++ Source/SurvivalGame/Public/UI/Widgets/Inventory/ItemContainerGrid.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonUserWidget.h"
+#include "ItemContainerGrid.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SURVIVALGAME_API UItemContainerGrid : public UCommonUserWidget
+{
+	GENERATED_BODY()
+};
